1c696b198f135dbb8a2914ea730ad58f3897c087
andyyablonskii <andyyablonskii@gmail.com>
2023-03-28 17:24:21 +0200

    aded header and styled it

---
+++ tu/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="headerConteiner">
+      <div className="headerContent">
+        <div className="headerText">Tu Dushka Website</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
