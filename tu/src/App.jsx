1a91819fef00420f2415e6f79d0dbf08d76b9cd3
andyyablonskii <andyyablonskii@gmail.com>
2023-03-29 10:33:42 +0200

    styled header and to does without checkbox styling and trash icon

--- tu/src/App.jsx
+++ tu/src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css';
-import Header from './Components/Header';
-import ToDoes from './Components/ToDoes';
+import "./App.css";
+import Header from "./Components/Header";
+import ToDoes from "./Components/ToDoes";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <ToDoes/>
-      
+      <Header />
+      <ToDoes />
     </div>
   );
 }
--- tu/src/Components/ToDo.jsx
+++ tu/src/Components/ToDo.jsx
@@ -6,16 +6,16 @@ const ToDo = (props) => {
         <div className="ToDoConteiner">
           <div key={index} className="ToDo">
             <div className="leftToDoPart">
-              <label className="toDoNameWRadio" id="nameChecker">
+              <label className="toDoNameWCheckbox" id="nameChecker">
                 <input type="checkbox" id="nameChecker" />
                 <div className="name">{item.name}</div>
               </label>
-
               <div className="description">{item.description}</div>
             </div>
 
             <div className="rightToDoPart">
               <div className="dateToDoes">{item.date.toDateString()}</div>
+              <div className="bin"></div>
             </div>
           </div>
         </div>
